114572b11078247d4a28e2f476f2e00a689fe011
Zeljko Minic <zeljko@bonfire.com>
2019-10-09 16:49:51 +0200

    Updates

---
+++ precache-manifest.112461e1249617d7b239a66f4df2c03a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef432e4f956298de2ee2b4f32189a8e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aabd32239b007ad41945",
+    "url": "/static/css/main.4c386de2.chunk.css"
+  },
+  {
+    "revision": "029ecc0871c6ac5a960b",
+    "url": "/static/js/2.379c837a.chunk.js"
+  },
+  {
+    "revision": "aabd32239b007ad41945",
+    "url": "/static/js/main.f24956db.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
